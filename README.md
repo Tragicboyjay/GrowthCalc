3b1125a5e22ec7adc0612bede106a74bef2938cb
Tragicboyjay <124944418+Tragicboyjay@users.noreply.github.com>
2023-09-27 13:22:58 -0400

    Added README

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# GrowthCalc
- The Compound Interest Calculator is a web-based financial tool that helps users estimate how their investments will grow over time with compound interest. This calculator takes into account various factors, including the initial investment, regular contributions, contribution frequency, interest rate, and the number of years for which the investment will grow
+Compound Interest Calculator - Growth Calc ™ 
+
+Deploy link: https://growthcalc-a5916.web.app/
+
+
+Overview
+
+The Compound Interest Calculator is a web-based financial tool that helps users estimate how their investments will grow over time with compound interest. This calculator takes into account various factors, including the initial investment, regular contributions, contribution frequency, interest rate, and the number of years for which the investment will grow.
+
+Features
+- Calculate the total value of your investment over a specified period.
+- Determine the total interest earned on your investment.
+- Flexible contribution frequency options, including weekly, bi-weekly, monthly, quarterly, and yearly.
+- Clear and user-friendly interface for inputting values and viewing results.
+- Assumptions section explaining the calculation methodology.
+
+How to Use
+
+1. Enter your initial investment amount.
+2. Specify the amount of your regular contributions.
+3. Select the contribution frequency (weekly, bi-weekly, monthly, quarterly, or yearly).
+4. Input the annual interest rate as a percentage.
+5. Indicate the number of years for your investment to grow.
+6. Click the "Calculate" button to see your results.
+
+Assumptions
+
+The calculator makes the following assumptions for its calculations:
+
+- Contributions are made at the end of each specified period.
+- Interest income compounds according to the selected compounding frequency.
+- Contributions are distributed evenly over the compounding period if the contribution frequency exceeds it. For example, if you make weekly contributions with monthly compounding, the calculations distribute the annual contributions evenly over the 12 months.
+
+Disclaimer
+
+Any results or calculations displayed on this site are provided for informational purposes only and should not be considered financial or legal advice. By using this calculator, you agree to our Website and Social Media Terms of Use and Privacy Policy.
+
+Developer
+
+This web-based Compound Interest Calculator was developed by Justice Manly.
+
+GitHub: https://github.com/Tragicboyjay
+LinkedIn: https://www.linkedin.com/in/justicemanly
+
+Feel free to explore the code and use the calculator for your financial planning needs!
---
+++ script.js
@@ -0,0 +1,165 @@
+const calculateButton = document.getElementById("calculateButton");
+
+const formContainer = document.getElementById('formContainer');
+
+const errorMessage = document.getElementById('errorMessage');
+
+const resultsGroup = document.getElementById('resultsGroup');
+
+const assumptionsButton = document.getElementById('assumptionsButton');
+
+const assumptionsText = document.getElementById('assumptionsText');
+
+function errorHandling(error){
+    errorMessage.innerText = error
+    errorMessage.style.display = 'block'
+    formContainer.classList.add('wrong-input');
+    setTimeout(() => {
+        formContainer.classList.remove('wrong-input');
+    }, 400)
+}
+
+let assumptions = false 
+
+function showAssumptions() {
+
+    if (assumptions) {
+        assumptionsButton.style.transform = 'rotateX(0deg)';
+        assumptionsText.style.animation = 'slideUp 0.6s ease-out';
+        setTimeout(function() {
+          assumptionsText.style.display = 'none';
+        }, 600);
+        assumptions = false;
+    }else{
+
+        assumptionsButton.style.transform = 'rotateX(180deg)';
+        assumptionsText.style.animation = 'slideDown 0.6s ease-in-out'
+        assumptionsText.style.display = 'block';
+        assumptions = true;
+    }
+    
+    
+}
+
+const compoundCalculation = () => {
+    const initialInvestment = parseFloat(document.getElementById("initialInvestment").value);
+    const regularContribution = parseFloat(document.getElementById("regularContribution").value);
+    const contributionFrequency = document.getElementById("contributionFrequency").value;
+    const interestRate = parseFloat(document.getElementById("interestRate").value);
+    const yearsToGrow = parseFloat(document.getElementById("yearsToGrow").value);
+
+    const yearsToGrowRange = document.getElementById("yearsToGrowRange");
+
+ 
+    const totalValue = document.getElementById("totalValue");
+    const totalInterest = document.getElementById("totalInterest");
+    const summary = document.getElementById("summary");
+
+    let yearlyContribution;
+
+    // Format Numbers Functions
+
+    function formatPercent(number) {
+        if (isNaN(number)) {
+            return "0%";
+        }else{
+            return number +'%'
+        }
+    }
+
+    function formatNumberAsCurrency(number) {
+        if (isNaN(number)) {
+          return "$0";
+        }
+        
+        const formattedNumber = parseFloat(number).toFixed(2);
+        
+        const [dollars, cents] = formattedNumber.split(".");
+      
+        const dollarsWithCommas = dollars.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      
+        const formattedCurrency = "$" + dollarsWithCommas + "." + cents;
+      
+        return formattedCurrency;
+    }
+
+    // Calculate Growth
+
+    switch(contributionFrequency){
+        
+        case 'weekly':
+            yearlyContribution = regularContribution *  52;
+            break;
+
+        case 'biWeekly':
+            yearlyContribution = regularContribution * 26;
+            break;
+
+        case 'monthly':
+            yearlyContribution = regularContribution * 12;
+            break;
+        
+        case 'quarterly':
+            yearlyContribution = regularContribution * 4;
+            break;
+
+        case 'yearly':
+            yearlyContribution = regularContribution;
+            break; 
+
+        default:
+            yearlyContribution = 0;
+            break;  
+
+    };
+
+    let fullValue= initialInvestment;
+    let moneyIn = initialInvestment 
+
+    const interestPercentage = (interestRate/100) + 1
+
+    for(let i = 1; i <= yearsToGrow; i++){
+        moneyIn += yearlyContribution;
+        fullValue +=  yearlyContribution;
+        fullValue *= interestPercentage;
+            
+    }
+
+    const interestEarned = fullValue - moneyIn
+
+
+    // Error Handling
+
+    if (interestRate > 30){
+
+        errorHandling('Interest rate may not exceed 30%');
+
+    }else if (initialInvestment == 0 && regularContribution == 0 && interestRate == 0 && yearsToGrow == 0) {
+
+        errorHandling('All values cannot be 0');
+
+    }
+    else if (initialInvestment < 0 || regularContribution < 0 || interestRate < 0 || yearsToGrow < 0){
+
+       errorHandling('Inputs cannot be negative');
+
+
+    }else {
+        // display results
+        errorMessage.style.animation = 'disappear 0.4s ease-out'
+        setTimeout( () => 
+        {errorMessage.style.display = 'none';
+        }, 400);
+        totalValue.innerText =  formatNumberAsCurrency(fullValue.toFixed(2));
+        totalInterest.innerText =  formatNumberAsCurrency(interestEarned.toFixed(2));
+        summary.innerText = 'Your initial investment of ' + formatNumberAsCurrency(initialInvestment) + ' plus your ' + contributionFrequency + ' investment of ' + formatNumberAsCurrency(regularContribution) + ' at an annualized interest rate of ' + formatPercent(interestRate) + ' will be worth '+ formatNumberAsCurrency(fullValue.toFixed(2)) + ' after ' + yearsToGrow + ' years when compounded yearly.'
+        resultsGroup.style.display = 'flex'
+
+    }
+       
+};
+
+
+calculateButton.addEventListener('click', compoundCalculation);
+assumptionsButton.addEventListener('click', showAssumptions);
+
